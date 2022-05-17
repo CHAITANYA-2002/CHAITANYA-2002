9ce3c86ee31e4482cd9517cb909351784371b5ea
CHAITANYA-2002 <72995227+CHAITANYA-2002@users.noreply.github.com>
2022-05-17 09:13:29 +0530

    lab

---
+++ programing java/lab/breader.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+/**
+ * breader
+ */
+public class breader {
+
+    public static void main(String[] args) {
+        throw Exception;
+
+        int a, b, c, ans;
+        BufferedReader br = new BufferedReader(br);
+        System.out.println("enter a :");
+        a = Integer.parseInt(br.readLine());
+
+        System.out.println("enter b :");
+        b = Integer.parseInt(br.readLine());
+
+        c = a + b;
+        System.out.println(c + " ");
+
+    }
+}
