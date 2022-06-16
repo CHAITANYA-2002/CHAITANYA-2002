8cb17829666eed69a272b9d629f67a6758da0375
CHAITANYA-2002 <72995227+CHAITANYA-2002@users.noreply.github.com>
2022-06-17 00:59:21 +0530

    Progs

---
+++ programing java/lab/superconstrucores.java
@@ -0,0 +1,23 @@
+/* superclass Person */
+class Person {
+    Person() {
+        System.out.println("Person class Constructor");
+    }
+}
+
+/* subclass Student extending the Person class */
+class Student extends Person {
+    Student() {
+        // invoke or call parent class constructor
+        super();
+
+        System.out.println("Student class Constructor");
+    }
+}
+
+/* Driver program to test */
+class Test2 {
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
--- programing java/lab/thread.java
+++ programing java/lab/thread.java
@@ -1,6 +1,10 @@
+class Multi extends Thread {
+    public void run() {
+        System.out.println("thread is running...");
+    }
 
-
-
-public class thread {
-    
+    public static void main(String args[]) {
+        Multi t1 = new Multi();
+        t1.start();
+    }
 }
